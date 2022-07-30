60f7b60ea87750974b381b277ab43d8c33f2210e
Akshaikh97 <shaikhkasim979@gmail.com>
2022-07-30 18:56:25 +0530

    Add project files.

---
+++ EFCoreRelationships/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreRelationships
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public List<Character> Characters { get; set; }
+    }
+}
